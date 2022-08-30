3ab6655c5c40bc01eeb1a2f09a624a9341d377f6
Anais <anaiszed447@gmail.com>
2022-08-30 02:30:27 -0400

    Added function to change tile color to red on hover

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ for(let i = 0; i < 256; i++){
     div.classList.add('grid-tile')
     mainDiv.appendChild(div)
 }
+
+function changeColor(e){
+    console.log(e)
+    e.target.style.backgroundColor = 'red'
+}
+
+const divTiles = document.querySelectorAll('.grid-tile')
+console.log(divTiles)
+
+divTiles.forEach((div) => {
+    div.addEventListener('mouseover', changeColor)
+});
+
+
+
+
+
+
