6d2e322ce21e2c4adfe65f8c0c742fc9dfa0346b
Anais <anaiszed447@gmail.com>
2022-08-30 22:37:42 -0400

    Created the logic to color the tiles

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 const mainDiv = document.querySelector('.main-div')
+let click = false
 
+//creating the tiles
 for(let i = 0; i < 256; i++){
-    const div = document.createElement('div')
-    div.classList.add('grid-tile')
-    mainDiv.appendChild(div)
+    const div = document.createElement('div');
+    div.classList.add('grid-tile');
+    mainDiv.appendChild(div);
 }
 
-function changeColor(e){
-    console.log(e)
-    e.target.style.backgroundColor = 'red'
+
+
+function startColor(){
+    if(click === true){
+         this.style.backgroundColor = "red";
+    }
 }
 
 const divTiles = document.querySelectorAll('.grid-tile')
 console.log(divTiles)
 
-divTiles.forEach((div) => {
-    div.addEventListener('mouseover', changeColor)
-});
+//event to color the tiles
+divTiles.forEach((div) =>{
+    div.addEventListener('mouseover', startColor);
+})
+
+//click event to toggle click var to true and false
+mainDiv.addEventListener('click', ()=>{
+    click = !click;
+})
+
 
 
 
